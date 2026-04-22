0add22c5f7bc99c37d075e80e9e3c0bc769e1179
FERELKING242 <111065891+ferelking242@users.noreply.github.com>
2026-04-22 12:36:58 +0100

    feat: add navShowLabelsProvider and navIconSizeProvider

---
+++ lib/modules/more/settings/appearance/providers/nav_display_state_provider.dart
@@ -0,0 +1,4 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final navShowLabelsProvider = StateProvider<bool>((ref) => true);
+final navIconSizeProvider = StateProvider<double>((ref) => 22);
