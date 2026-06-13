1a4e9129fe5892b0ace54a2992c9429d8434a2d4
FERELKING242 <111065891+ferelking242@users.noreply.github.com>
2026-06-13 03:42:26 +0100

    fix: add flutter_riverpod import for Provider<bool>

--- lib/modules/more/settings/browse/providers/browse_state_provider.dart
+++ lib/modules/more/settings/browse/providers/browse_state_provider.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:http/http.dart';
 import 'package:watchtower/main.dart';
 import 'package:watchtower/models/manga.dart';
