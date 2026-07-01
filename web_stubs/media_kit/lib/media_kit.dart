d3e783e1eca248b4218d953cbba7dcb63a919907
FERELKING242 <111065891+ferelking242@users.noreply.github.com>
2026-07-01 15:30:57 +0100

    fix(stubs): comprehensive media_kit, sqlite3, drift, newpipe, metadata_god, home_widget fixes

--- web_stubs/media_kit/lib/media_kit.dart
+++ web_stubs/media_kit/lib/media_kit.dart
@@ -1,172 +1,204 @@
 import 'dart:async';
-import 'package:flutter/foundation.dart';
+  import 'package:flutter/foundation.dart';
 
-class MediaKit {
-  static void ensureInitialized() {}
-}
-
-class Media {
-  final String uri;
-  final Map<String, String>? httpHeaders;
-  const Media(this.uri, {this.httpHeaders});
-}
-
-class Playlist {
-  final List<Media> medias;
-  final int index;
-  const Playlist(this.medias, {this.index = 0});
-}
-
-class PlayerConfiguration {
-  final bool ready;
-  final String? title;
-  final int bufferSize;
-  final bool pitch;
-  final String? vo;
-  final bool protocolWhitelist;
-  const PlayerConfiguration({
-    this.ready = false,
-    this.title,
-    this.bufferSize = 32 * 1024 * 1024,
-    this.pitch = true,
-    this.protocolWhitelist = true,
-    this.vo,
-  });
-}
-
-class PlayerState {
-  final Media? playlist;
-  final bool playing;
-  final Duration position;
-  final Duration duration;
-  final double volume;
-  final double rate;
-  final double pitch;
-  final bool buffering;
-  final Duration buffer;
-  final String? error;
-  final List<AudioTrack> audioTracks;
-  final List<VideoTrack> videoTracks;
-  final List<SubtitleTrack> subtitleTracks;
-  final AudioTrack? track;
-  final bool completed;
-  final double width;
-  final double height;
-  final List<String> subtitle;
-  const PlayerState({
-    this.playlist,
-    this.playing = false,
-    this.position = Duration.zero,
-    this.duration = Duration.zero,
-    this.volume = 100.0,
-    this.rate = 1.0,
-    this.pitch = 1.0,
-    this.buffering = false,
-    this.buffer = Duration.zero,
-    this.error,
-    this.audioTracks = const [],
-    this.videoTracks = const [],
-    this.subtitleTracks = const [],
-    this.track,
-    this.completed = false,
-    this.width = 0,
-    this.height = 0,
-    this.subtitle = const [],
-  });
-}
-
-class PlayerStream {
-  final Stream<Media?> playlist = const Stream.empty();
-  final Stream<bool> playing = const Stream.empty();
-  final Stream<Duration> position = const Stream.empty();
-  final Stream<Duration> duration = const Stream.empty();
-  final Stream<double> volume = const Stream.empty();
-  final Stream<double> rate = const Stream.empty();
-  final Stream<double> pitch = const Stream.empty();
-  final Stream<bool> buffering = const Stream.empty();
-  final Stream<Duration> buffer = const Stream.empty();
-  final Stream<String?> error = const Stream.empty();
-  final Stream<List<AudioTrack>> audioTracks = const Stream.empty();
-  final Stream<List<VideoTrack>> videoTracks = const Stream.empty();
-  final Stream<List<SubtitleTrack>> subtitleTracks = const Stream.empty();
-  final Stream<bool> completed = const Stream.empty();
-  final Stream<double> width = const Stream.empty();
-  final Stream<double> height = const Stream.empty();
-  final Stream<List<int>> log = const Stream.empty();
-  final Stream<List<String>> subtitle = const Stream.empty();
-    final Stream<PlaylistMode> playlistMode = const Stream.empty();
+  class MediaKit {
+    static void ensureInitialized() {}
   }
 
-// Alias kept for backwards compat
-typedef PlayerStreams = PlayerStream;
+  class AudioDevice {
+    final String name;
+    final String description;
+    const AudioDevice(this.name, this.description);
+    static AudioDevice get auto => const AudioDevice('auto', 'Auto');
+    @override
+    bool operator ==(Object other) => other is AudioDevice && other.name == name;
+    @override
+    int get hashCode => name.hashCode;
+  }
 
-class AudioTrack {
-  final String id;
-  final String? title;
-  final String? language;
-  const AudioTrack(this.id, {this.title, this.language});
-  static AudioTrack get auto => const AudioTrack('auto');
-  static AudioTrack get no => const AudioTrack('no');
-}
+  enum MPVLogLevel { none, fatal, error, warn, info, status, v, debug, trace }
 
-class VideoTrack {
-  final String id;
-  final String? title;
-  final String? language;
-  const VideoTrack(this.id, {this.title, this.language});
-  static VideoTrack get auto => const VideoTrack('auto');
-  static VideoTrack get no => const VideoTrack('no');
-}
+  class Media {
+    final String uri;
+    final Map<String, String>? httpHeaders;
+    final Map<String, dynamic>? extras;
+    const Media(this.uri, {this.httpHeaders, this.extras});
+  }
 
-class SubtitleTrack {
-  final String? id;
-  final String? title;
-  final String? language;
-  final String? uri;
-  const SubtitleTrack(this.id, {this.title, this.language, this.uri});
-  static SubtitleTrack get auto => const SubtitleTrack('auto');
-  static SubtitleTrack get no => const SubtitleTrack('no');
-  factory SubtitleTrack.uri(String uri, {String? title, String? language}) =>
-      SubtitleTrack(null, title: title, language: language, uri: uri);
-  factory SubtitleTrack.data(String data, {String? title, String? language}) =>
-      SubtitleTrack(null, title: title, language: language);
-}
+  class Playlist {
+    final List<Media> medias;
+    final int index;
+    const Playlist(this.medias, {this.index = 0});
+  }
 
-enum PlaylistMode { none, single, loop }
+  class PlayerConfiguration {
+    final bool ready;
+    final String? title;
+    final int bufferSize;
+    final bool pitch;
+    final String? vo;
+    final bool protocolWhitelist;
+    final MPVLogLevel? logLevel;
+    final bool async;
+    const PlayerConfiguration({
+      this.ready = false,
+      this.title,
+      this.bufferSize = 32 * 1024 * 1024,
+      this.pitch = true,
+      this.protocolWhitelist = true,
+      this.vo,
+      this.logLevel,
+      this.async = false,
+    });
+  }
+
+  class PlayerState {
+    final Playlist playlist;
+    final bool playing;
+    final Duration position;
+    final Duration duration;
+    final double volume;
+    final double rate;
+    final double pitch;
+    final bool buffering;
+    final Duration buffer;
+    final String? error;
+    final List<AudioTrack> audioTracks;
+    final List<VideoTrack> videoTracks;
+    final List<SubtitleTrack> subtitleTracks;
+    final AudioTrack? track;
+    final bool completed;
+    final double width;
+    final double height;
+    final List<String> subtitle;
+    final bool shuffle;
+    final AudioDevice audioDevice;
+    final List<AudioDevice> audioDevices;
+    final PlaylistMode playlistMode;
+    const PlayerState({
+      this.playlist = const Playlist([]),
+      this.playing = false,
+      this.position = Duration.zero,
+      this.duration = Duration.zero,
+      this.volume = 100.0,
+      this.rate = 1.0,
+      this.pitch = 1.0,
+      this.buffering = false,
+      this.buffer = Duration.zero,
+      this.error,
+      this.audioTracks = const [],
+      this.videoTracks = const [],
+      this.subtitleTracks = const [],
+      this.track,
+      this.completed = false,
+      this.width = 0,
+      this.height = 0,
+      this.subtitle = const [],
+      this.shuffle = false,
+      this.audioDevice = const AudioDevice('auto', 'Auto'),
+      this.audioDevices = const [],
+      this.playlistMode = PlaylistMode.none,
+    });
+  }
+
+  class PlayerStream {
+    final Stream<Playlist> playlist = const Stream.empty();
+    final Stream<bool> playing = const Stream.empty();
+    final Stream<Duration> position = const Stream.empty();
+    final Stream<Duration> duration = const Stream.empty();
+    final Stream<double> volume = const Stream.empty();
+    final Stream<double> rate = const Stream.empty();
+    final Stream<double> pitch = const Stream.empty();
+    final Stream<bool> buffering = const Stream.empty();
+    final Stream<Duration> buffer = const Stream.empty();
+    final Stream<String> error = const Stream.empty();
+    final Stream<List<AudioTrack>> audioTracks = const Stream.empty();
+    final Stream<List<VideoTrack>> videoTracks = const Stream.empty();
+    final Stream<List<SubtitleTrack>> subtitleTracks = const Stream.empty();
+    final Stream<bool> completed = const Stream.empty();
+    final Stream<double> width = const Stream.empty();
+    final Stream<double> height = const Stream.empty();
+    final Stream<List<int>> log = const Stream.empty();
+    final Stream<List<String>> subtitle = const Stream.empty();
+    final Stream<PlaylistMode> playlistMode = const Stream.empty();
+    final Stream<bool> shuffle = const Stream.empty();
+    final Stream<List<AudioDevice>> audioDevices = const Stream.empty();
+    final Stream<AudioDevice> audioDevice = const Stream.empty();
+  }
+
+  typedef PlayerStreams = PlayerStream;
+
+  class AudioTrack {
+    final String id;
+    final String? title;
+    final String? language;
+    const AudioTrack(this.id, {this.title, this.language});
+    static AudioTrack get auto => const AudioTrack('auto');
+    static AudioTrack get no => const AudioTrack('no');
+  }
+
+  class VideoTrack {
+    final String id;
+    final String? title;
+    final String? language;
+    const VideoTrack(this.id, {this.title, this.language});
+    static VideoTrack get auto => const VideoTrack('auto');
+    static VideoTrack get no => const VideoTrack('no');
+  }
+
+  class SubtitleTrack {
+    final String? id;
+    final String? title;
+    final String? language;
+    final String? uri;
+    const SubtitleTrack(this.id, {this.title, this.language, this.uri});
+    static SubtitleTrack get auto => const SubtitleTrack('auto');
+    static SubtitleTrack get no => const SubtitleTrack('no');
+    factory SubtitleTrack.uri(String uri, {String? title, String? language}) =>
+        SubtitleTrack(null, title: title, language: language, uri: uri);
+    factory SubtitleTrack.data(String data, {String? title, String? language}) =>
+        SubtitleTrack(null, title: title, language: language);
+  }
+
+  enum PlaylistMode { none, single, loop }
 
   abstract class PlatformPlayer {
-  Future<void> setProperty(String property, String value) async {}
-  Future<void> command(List<String> cmd) async {}
-}
+    Future<void> setProperty(String property, String value) async {}
+    Future<void> command(List<String> cmd) async {}
+  }
 
-class NativePlayer extends PlatformPlayer {}
+  class NativePlayer extends PlatformPlayer {}
 
-class Player {
-  final PlayerConfiguration configuration;
-  final PlayerState state = const PlayerState();
-  final PlayerStream stream = PlayerStream();
-  PlatformPlayer? get platform => NativePlayer();
+  class Player {
+    final PlayerConfiguration configuration;
+    final PlayerState state = const PlayerState();
+    final PlayerStream stream = PlayerStream();
+    PlatformPlayer? get platform => NativePlayer();
 
-  Player({this.configuration = const PlayerConfiguration()});
+    Player({this.configuration = const PlayerConfiguration()});
 
-  Future<void> open(dynamic playable, {bool play = true}) async {}
-  Future<void> play() async {}
-  Future<void> pause() async {}
-  Future<void> playOrPause() async {}
-  Future<void> stop() async {}
-  Future<void> next() async {}
-  Future<void> previous() async {}
-  Future<void> jump(int index) async {}
-  Future<void> seek(Duration duration) async {}
-  Future<void> setRate(double rate) async {}
-  Future<void> setPitch(double pitch) async {}
-  Future<void> setVolume(double volume) async {}
-  Future<void> setAudioTrack(AudioTrack track) async {}
-  Future<void> setVideoTrack(VideoTrack track) async {}
-  Future<void> setSubtitleTrack(SubtitleTrack track) async {}
-  Future<void> setLoopMode(PlaylistMode playlistMode) async {}
+    Future<void> open(dynamic playable, {bool play = true}) async {}
+    Future<void> play() async {}
+    Future<void> pause() async {}
+    Future<void> playOrPause() async {}
+    Future<void> stop() async {}
+    Future<void> next() async {}
+    Future<void> previous() async {}
+    Future<void> jump(int index) async {}
+    Future<void> seek(Duration duration) async {}
+    Future<void> setRate(double rate) async {}
+    Future<void> setPitch(double pitch) async {}
+    Future<void> setVolume(double volume) async {}
+    Future<void> setAudioTrack(AudioTrack track) async {}
+    Future<void> setVideoTrack(VideoTrack track) async {}
+    Future<void> setSubtitleTrack(SubtitleTrack track) async {}
+    Future<void> setLoopMode(PlaylistMode playlistMode) async {}
+    Future<void> setPlaylistMode(PlaylistMode playlistMode) async {}
+    Future<void> setShuffle(bool shuffle) async {}
+    Future<void> setAudioDevice(AudioDevice device) async {}
     Future<void> add(Media media) async {}
-  Future<void> remove(int index) async {}
-  Future<void> move(int from, int to) async {}
-  Future<void> dispose() async {}
-}
+    Future<void> remove(int index) async {}
+    Future<void> move(int from, int to) async {}
+    Future<void> dispose() async {}
+  }
+  
--- web_stubs/metadata_god/lib/metadata_god.dart
+++ web_stubs/metadata_god/lib/metadata_god.dart
@@ -1,29 +1,30 @@
-
 class Picture {
-  final String? mimeType;
-  final List<int>? data;
-  const Picture({this.mimeType, this.data});
-}
-class Metadata {
-  final String? title;
-  final String? artist;
-  final String? album;
-  final int? trackNumber;
-  final int? trackTotal;
-  final int? discNumber;
-  final int? discTotal;
-  final String? year;
-  final String? genre;
-  final String? comment;
-  final int? durationMs;
-  final int? bitrate;
-  final int? sampleRate;
-  final int? channels;
-  final Picture? picture;
-  const Metadata({this.title, this.artist, this.album, this.trackNumber, this.trackTotal, this.discNumber, this.discTotal, this.year, this.genre, this.comment, this.durationMs, this.bitrate, this.sampleRate, this.channels, this.picture});
-}
-class MetadataGod {
-  static Future<void> initialize() async {}
-  static Future<Metadata> readMetadata({required String file, bool getImage = false}) async => const Metadata();
-  static Future<void> writeMetadata({required String file, required Metadata metadata}) async {}
-}
+    final String? mimeType;
+    final List<int>? data;
+    const Picture({this.mimeType, this.data});
+  }
+  class Metadata {
+    final String? title;
+    final String? artist;
+    final String? albumArtist;
+    final String? album;
+    final int? trackNumber;
+    final int? trackTotal;
+    final int? discNumber;
+    final int? discTotal;
+    final String? year;
+    final String? genre;
+    final String? comment;
+    final int? durationMs;
+    final int? bitrate;
+    final int? sampleRate;
+    final int? channels;
+    final Picture? picture;
+    const Metadata({this.title, this.artist, this.albumArtist, this.album, this.trackNumber, this.trackTotal, this.discNumber, this.discTotal, this.year, this.genre, this.comment, this.durationMs, this.bitrate, this.sampleRate, this.channels, this.picture});
+  }
+  class MetadataGod {
+    static Future<void> initialize() async {}
+    static Future<Metadata> readMetadata({required String file, bool getImage = false}) async => const Metadata();
+    static Future<void> writeMetadata({required String file, required Metadata metadata}) async {}
+  }
+  
