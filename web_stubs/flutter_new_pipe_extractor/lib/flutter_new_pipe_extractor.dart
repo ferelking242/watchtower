ed72436273faf221509b655959d7252c5dc142ec
FERELKING242 <111065891+ferelking242@users.noreply.github.com>
2026-07-01 14:30:10 +0100

    web: vercel deploy + 10 new stubs + compat report

---
+++ web_stubs/flutter_new_pipe_extractor/lib/flutter_new_pipe_extractor.dart
@@ -0,0 +1,7 @@
+
+class NewPipeExtractor {
+  static Future<dynamic> getStream(String url) async => null;
+  static Future<List<dynamic>> getRelatedStreams(String url) async => [];
+  static Future<dynamic> search(String query, List<dynamic> filters) async => null;
+  static Future<List<dynamic>> getChannelVideos(String channelUrl) async => [];
+}
