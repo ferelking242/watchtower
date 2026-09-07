2aa6d610910954ea1271fc9b6ea9e716ea4f4dba
Devix16496 <63040952-devix16496@users.noreply.replit.com>
2026-09-07 10:26:09 +0000

    Use current editor divider API

--- lib/modules/browse/extension/edit_code.dart
+++ lib/modules/browse/extension/edit_code.dart
@@ -380,7 +380,7 @@ class _CodeEditorPageState extends ConsumerState<CodeEditorPage> {
                                 ],
                               );
                             },
-                        separator: Container(
+                        leadingDivider: Container(
                           width: 1,
                           color: context.dynamicThemeColor.withValues(
                             alpha: 0.3,
