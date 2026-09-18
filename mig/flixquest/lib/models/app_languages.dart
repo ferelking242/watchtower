c6484740cd4cf0a5dcf107b84a744631b7d95c5c
royija7991 <48303460-royija7991@users.noreply.replit.com>
2026-09-18 18:22:47 +0000

    Move MIG preview into standalone repository

--- mig/flixquest/lib/models/app_languages.dart
+++
@@ -1,9 +0,0 @@
-class AppLanguages {
-  AppLanguages(
-      {required this.languageFlag,
-      required this.languageName,
-      required this.languageCode});
-  String languageName;
-  String languageFlag;
-  String languageCode;
-}
